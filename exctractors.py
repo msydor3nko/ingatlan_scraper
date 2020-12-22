672da9c0fe1a7b5d1aa21c51407bcd0559d7c520
Mykhailo Sydorenko <msydor3nko@gmail.com>
2020-12-22 17:00:24 +0200

    fix OOP style

--- exctractors.py
+++ exctractors.py
@@ -1,51 +1,63 @@
 import requests
 from parsel import Selector
 
-from constants import URL_DOMAIN
+from constants import URL_DOMAIN, HEADERS
 from constants import XPATH_PAGINATOR_TOTAL_PAGES, XPATH_ITEM_URLS_IN_CATALOG
 from constants import XPATH_ITEM_TITLE, XPATH_ITEM_DESCRIPTION, XPATH_ITEM_PRICE, XPATH_ITEM_ROOMS, XPATH_ITEM_AREA, XPATH_ITEM_PARAMETERS
 
 
-def exctract_paginator_pages_in_catalog(tree: Selector) -> int:
-    total_pages_text = tree.xpath(XPATH_PAGINATOR_TOTAL_PAGES).extract_first()
-    return int(total_pages_text.split(" ")[3]) if total_pages_text else 0
+class Scraper(object):
 
-def exctract_apart_urls_in_catalog(tree: Selector) -> list:
-    apart_urls = tree.xpath(XPATH_APART_URLS).extract()
-    if not apart_urls:
-        return []
-    return [URL_DOMAIN + apart_url for apart_url in apart_urls]
+    def __init__(self, url):
+        self.url = url
 
-def exctract_apart_page_data(tree: Selector) -> dict:
-    result = {}
-    
-    title = tree.xpath(XPATH_ITEM_TITLE).extract_first()
-    
-    price = tree.xpath(XPATH_ITEM_PRICE).extract_first()
-    price = int(price.strip(" Ft").replace(" ", ""))
+    def get_html_page(self) -> Selector:
+        response = requests.request("GET", self.url, headers=HEADERS)
+        return Selector(text=response.text) if response.ok else None
 
-    rooms = tree.xpath(XPATH_ITEM_ROOMS).extract_first()
-    rooms = int(rooms.strip()) if rooms else None
+    @staticmethod
+    def exctract_paginator_pages_in_catalog(tree: Selector) -> int:
+        total_pages_text = tree.xpath(XPATH_PAGINATOR_TOTAL_PAGES).extract_first()
+        return int(total_pages_text.split(" ")[3]) if total_pages_text else 0
     
-    area = tree.xpath(XPATH_ITEM_AREA).extract_first()
-    area = int(area.strip(" m²"))
+    @staticmethod
+    def exctract_apart_urls_in_catalog(tree: Selector) -> list:
+        apart_urls = tree.xpath(XPATH_APART_URLS).extract()
+        if not apart_urls:
+            return []
+        return [URL_DOMAIN + apart_url for apart_url in apart_urls]
+    
+    @staticmethod
+    def exctract_apart_data(tree: Selector) -> dict:
+        result = {}
+        
+        title = tree.xpath(XPATH_ITEM_TITLE).extract_first()
+        
+        price = tree.xpath(XPATH_ITEM_PRICE).extract_first()
+        price = int(price.strip(" Ft").replace(" ", ""))
 
-    description = tree.xpath(XPATH_ITEM_DESCRIPTION).extract_first()
-    description = description.strip() if description else None
-    
-    parameters = tree.xpath(XPATH_ITEM_PARAMETERS).extract()
-    parameters = dict(zip(parameters[0::2], parameters[1::2]))
-    for key, val in parameters.items():
-        if val == "nincs megadva":
-            parameters[key] = None
-    
-    result.update(
-        title=title,
-        price=price,
-        rooms=rooms,
-        area=area,
-        description=description,
-        **parameters,
-    )
+        rooms = tree.xpath(XPATH_ITEM_ROOMS).extract_first()
+        rooms = int(rooms.strip()) if rooms else None
+        
+        area = tree.xpath(XPATH_ITEM_AREA).extract_first()
+        area = int(area.strip(" m²"))
 
-    return result
+        description = tree.xpath(XPATH_ITEM_DESCRIPTION).extract_first()
+        description = description.strip() if description else None
+        
+        parameters = tree.xpath(XPATH_ITEM_PARAMETERS)
+        parameters = dict(tr.xpath("td/text()").extract() for tr in parameters)
+        for key, val in parameters.items():
+            if val == "nincs megadva":
+                parameters[key] = None
+        
+        apart_data = dict(
+            title=title,
+            price=price,
+            rooms=rooms,
+            area=area,
+            **parameters,
+            description=description,
+        )
+
+        return apart_data
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 from parsel import Selector
 
-import exctractors
+from exctractors import Scraper
 from constants import URL_APART_CATALOG
 from constants import XPATH_PAGINATOR_TOTAL_PAGES, XPATH_ITEM_URLS_IN_CATALOG
 from constants import XPATH_ITEM_TITLE
@@ -14,11 +14,10 @@ def get_page(url: str) -> Selector:
      
 
 if __name__ == "__main__":
-    url = 'https://ingatlan.com/vii-ker/kiado+lakas/tegla-epitesu-lakas/31927559'
-    tree = get_page(url)
+    URL = 'https://ingatlan.com/vii-ker/kiado+lakas/tegla-epitesu-lakas/31927559'
 
-    # paginator = exctractors.exctract_paginator_pages_in_catalog(tree)
-    # aparts_urls = exctractors.exctract_apart_urls_in_catalog(tree)
-    result = exctractors.exctract_apart_page_data(tree)
+    scraper = Scraper(URL)
+    tree = scraper.get_html_page()
+    result = scraper.exctract_apart_data(tree)
 
     print(result)
